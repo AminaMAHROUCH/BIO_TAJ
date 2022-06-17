963877bd65412a458ac114e5980600a33f971d6d
a.mahrouch <aminamahrouch042@gmail.com>
2022-06-17 16:44:05 +0100

    fix

--- resources/views/updatevisite.blade.php
+++ resources/views/updatevisite.blade.php
@@ -4,44 +4,46 @@
     <div class="row">
         <div class="col-lg-6">
             <label for="">الوزن</label>
-            <input class="form-control" type="number" name="poid">
+            <input class="form-control" value="{{$visite->poid}}" type="number" name="poid">
         </div>
         <div class="col-lg-6">
         <label for="">الضغط</label>
-            <input class="form-control" type="number" name="tension">
+            <input class="form-control" value="{{$visite->tension}}" type="number" name="tension">
         </div>
     </div>
     <div class="row">
         <div class="col-lg-6">
         <label for="">نبض القلب</label>
-            <input class="form-control" type="number" name="battement_coeur">
+            <input class="form-control" value="{{$visite->battement_coeur}}" type="number" name="battement_coeur">
         </div>
         <div class="col-lg-6">
         <label for="">مستوى السكري</label>
-            <input class="form-control" type="number" name="niveau_diabete">
+            <input class="form-control" value="{{$visite->niveau_diabete}}" type="number" name="niveau_diabete">
         </div>
     </div>
     <div class="row">
         <div class="col-lg-6">
         <label for="">الأدوية المستعملة</label>
-            <input class="form-control" type="text" name="medicaments_utilises">
+            <input class="form-control" value="{{$visite->medicaments_utilises}}" type="text" name="medicaments_utilises">
         </div>
     </div>
     <div class="row">
         <div class="col-lg-6">
         <label for="">المرض الحالي</label>
-            <input class="form-control" type="text" name="maladies_actuelles">
+            <input class="form-control" value="{{$visite->maladies_actuelles}}" type="text" name="maladies_actuelles">
         </div>
         <div class="col-lg-6">
         <label for="">علاج</label>
-            <input class="form-control" type="text" name="remede">
+            <input class="form-control" value="{{$visite->remede}}" type="text" name="remede">
         </div>
     </div>
     <div class="row">
         <div class="col-lg-12">
         <label for="">الأعراض</label>
-
-        <textarea name="sympthomes" class="form-control" cols="30" rows="5"></textarea>
+        <textarea name="sympthomes" class="form-control"  cols="30" rows="5">{{$visite->sympthomes}}</textarea>
         </div>
+    </div>
+    <div>
+        <button type="submit" class="btn btn-success">تعديل</button>
     </div>
 </form>
